8eb4d40fc1158ea56d004af0fe1994c006b940bf
luvvas <lucasmachado1@ufpr.br>
2022-12-03 09:25:59 -0300

    feat: add VerifyPasswordHash method and Login method

--- jwtAuthentication/Controllers/AuthController.cs
+++ jwtAuthentication/Controllers/AuthController.cs
@@ -12,7 +12,7 @@ namespace jwtAuthentication.Controllers
 		public static User user = new User();
 
 		[HttpPost("register")]
-		public async Task<ActionResult<User>> Register(UserDto request)
+		public async Task<ActionResult<User>> Register([FromBody] UserDto request)
 		{
 			// passwordHash e passwordSalt atualizam por referência quando CreatePasswordHash() é executado
 			CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
@@ -24,12 +24,36 @@ namespace jwtAuthentication.Controllers
 			return Ok(user);
 		}
 
+		[HttpPost("login")]
+		public async Task<ActionResult<string>> Login([FromBody] UserDto request)
+		{
+			if(user.Username != request.Username)
+			{
+				return BadRequest("User not found.");
+			}
+			if(!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+			{
+				return BadRequest("Wrong password.");
+			}
+
+			return Ok("Deu certo.");
+		}
+
 		private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
 		{
 			using (var hmac = new HMACSHA512())
 			{
 				passwordSalt = hmac.Key;
 				passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+			}
+		}
+
+		private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+		{
+			using(var hmac = new HMACSHA512(passwordSalt))
+			{
+				var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+				return computedHash.SequenceEqual(passwordHash);
 			}
 		}
 	}
