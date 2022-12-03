3b4ac329cd3f6f0f5714f0a2b3b5b15c387be81e
luvvas <lucasmachado1@ufpr.br>
2022-12-03 08:45:33 -0300

    feat: add User model

---
+++ jwtAuthentication/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace jwtAuthentication.Models
+{
+	public class User
+	{
+		// Add an id when start using database
+		public string Username { get; set; }
+		public byte[] PasswordHash { get; set; }
+		public byte[] PasswordSalt { get; set; }
+	}
+}
