12f34800482eccebf96f8f36d7276e450e7088f8
luvvas <lucasmachado1@ufpr.br>
2022-12-04 23:37:11 -0300

    feat: add ServiceResponse model

---
+++ jwtAuthentication/Models/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace jwtAuthentication.Models
+{
+	public class ServiceResponse
+	{
+		public T? Data { get; set; }
+		public bool Success { get; set; } = true;
+		public string Message {  get; set; } = string.Empty;
+	}
+}
