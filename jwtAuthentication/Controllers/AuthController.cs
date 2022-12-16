527bb5e451b08179f2be5df42d3f306f33abde7f
luvvas <lucasmachado1@ufpr.br>
2022-12-16 19:21:39 -0300

    feat: add GenerateRefreshToken method

--- jwtAuthentication/Controllers/AuthController.cs
+++ jwtAuthentication/Controllers/AuthController.cs
@@ -95,6 +95,18 @@ namespace jwtAuthentication.Controllers
       .ToArray();
     }
 
+    private RefreshToken GenerateRefreshToken()
+    {
+      var refreshToken = new RefreshToken
+      {
+        Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+        Expires = DateTime.UtcNow.AddDays(7),
+        Created = DateTime.Now
+      };
+
+      return refreshToken;
+    }
+
     // Need to review
     private string CreateToken(User user)
     {
--- jwtAuthentication/Models/User.cs
+++ jwtAuthentication/Models/User.cs
@@ -8,7 +8,7 @@
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
 
-    public string RefeshToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public DateTime TokenCreated { get; set; }
     public DateTime TokenExpires { get; set; }
   }
